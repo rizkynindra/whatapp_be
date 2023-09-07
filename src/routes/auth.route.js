c0790ccfcb67db552dec7d891f16c04d0a70f2a6
rizky nindra <rizkynindra.sukma@bpjsketenagakerjaan.go.id>
2023-09-07 19:28:20 +0700

    Update auth.route.js

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -6,11 +6,17 @@ import {
   refreshToken,
   register,
 } from "../controllers/auth.controller.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router.route("/register").post(trimRequest.all, register);
 router.route("/login").post(trimRequest.all, login);
 router.route("/logout").post(trimRequest.all, logout);
 router.route("/refreshtoken").post(trimRequest.all, refreshToken);
+router
+.route("/testingauthMiddleware")
+.get(trimRequest.all, authMiddleware, (req,res)=>{
+  res.send(req.user);
+});
 
 export default router;
