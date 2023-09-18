35c2e7c80557b0d4d631a131990d184c723394c3
rizky nindra <rizkynindra.sukma@bpjsketenagakerjaan.go.id>
2023-09-18 12:05:38 +0700

    Update conversation.controller.js

--- src/controllers/conversation.controller.js
+++ src/controllers/conversation.controller.js
@@ -7,35 +7,48 @@ export const create_open_conversation = async (req, res, next) => {
   try{
     const sender_id = req.user.userId; 
     const { receiver_id, isGroup } = req.body;
-    //check if receiver_id is provided
-    if (!receiver_id) {
-      logger.error(
-        "please provide the user id you wanna start a conversation with !"
+    if (isGroup == false) {
+      //check if receiver_id is provided
+      if (!receiver_id) {
+        logger.error(
+          "please provide the user id you wanna start a conversation with !"
+        );
+        throw createHttpError.BadGateway("Oops...Something went wrong !");
+      }
+      //check if chat exists
+      const existed_conversation = await doesConversationExist(
+        sender_id,
+        receiver_id,
+        false
       );
-      throw createHttpError.BadGateway("Oops...Something went wrong !");
-    }
-    //check if chat exists
-    const existed_conversation = await doesConversationExist(
-      sender_id,
-      receiver_id
-    );
-    if (existed_conversation) {
-      res.json(existed_conversation);
+      if (existed_conversation) {
+        res.json(existed_conversation);
+      } else {
+        // let receiver_user = await User(receiver_id);
+        let convoData = {
+          name: "conversation name",
+          picture: "conversation picture",
+          isGroup: false,
+          users: [sender_id, receiver_id],
+        };
+        const newConvo = await createConversation(convoData);
+        const populatedConvo = await populateConversation(
+          newConvo._id,
+          "users",
+          "-password"
+        );
+        res.status(200).json(populatedConvo);
+      }
     } else {
-      let receiver_user = await findUser(receiver_id);
-      let convoData = {
-        name: receiver_user.name,
-        picture: "conversation picture",
-        isGroup: false,
-        users: [sender_id, receiver_id],
-      };
-      const newConvo = await createConversation(convoData);
-      const populatedConvo = await populateConversation(
-        newConvo._id,
-        "users",
-        "-password"
+      console.log();
+      //it's a group chat
+      //check if group chat exists
+      const existed_group_conversation = await doesConversationExist(
+        "",
+        "",
+        isGroup
       );
-      res.status(200).json(populatedConvo);
+      res.status(200).json(existed_group_conversation);
     }
   } catch (error){
     next(error);
@@ -47,6 +60,38 @@ export const getConversations = async (req, res, next) => {
     const user_id = req.user.userId;
     const conversations = await getUserConversations(user_id);
     res.status(200).json(conversations);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createGroup = async (req, res, next) => {
+  const { name, users } = req.body;
+  //add current user to users
+  users.push(req.user.userId);
+  if (!name || !users) {
+    throw createHttpError.BadRequest("Please fill all fields.");
+  }
+  if (users.length < 2) {
+    throw createHttpError.BadRequest(
+      "Atleast 2 users are required to start a group chat."
+    );
+  }
+  let convoData = {
+    name,
+    users,
+    isGroup: true,
+    admin: req.user.userId,
+    picture: process.env.DEFAULT_GROUP_PICTURE,
+  };
+  try {
+    const newConvo = await createConversation(convoData);
+    const populatedConvo = await populateConversation(
+      newConvo._id,
+      "users admin",
+      "-password"
+    );
+    res.status(200).json(populatedConvo);
   } catch (error) {
     next(error);
   }
